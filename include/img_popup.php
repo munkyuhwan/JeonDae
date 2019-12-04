108ae8e2866739d19e5763fd26a93c3a5bc35e98
munkyuhwan <munkyuhwan@gmail.com>
2019-12-04 10:47:41 +0900

    6. 게시물의 이미지 확대 보기

--- include/img_popup.php
+++ include/img_popup.php
@@ -2,45 +2,45 @@
     var popImg = "<li class=\"swiper-slide\" >"+
                  "   <img src=\"../thumb/thumb.php?src={0}\" alt=\"\">"+
                  "</li>"
-    function setImages(report_idx) {
-        swiper = new Swiper('.swiper-container',{
-            effect: "slide",
-            loop: true,
-            navigation:{ nextEl: ".slide_next_btn", prevEl: ".slide_prev_btn" },
-            autoplay: { delay: 3000 },
-            on:{ slideChange: function(){
-                $(".main_visual").removeClass("bounce");
-                setTimeout(function() {
-                    $(".main_visual").addClass("bounce")  }, 200); }
-            },
-        });
+    function setImages(report_idx, likeCnt, commentCnt) {
+
         $.ajax({
             url:"get_images.php",
             method:"POST",
             data:{"report_idx":report_idx},
             success:function(response){
-                $(".pop_call").on("click",function(){
-                    var name = $(this).attr("data-pop");
-                    $(".popup."+name).fadeIn();
-                    $(".mask").fadeIn();
-                    $("html").addClass("scroll_no");
-                    $(".snb").removeClass("snb_on");
-                    swiper.update();
-                });
 
                 try {
+                    $('#pop_like_cnt').html(likeCnt);
+                    $('#pop_comment_cnt').html(commentCnt);
                     var res = JSON.parse(response);
                     var str = "";
                     for(let obj of res) {
-                        console.log(obj)
                         str += popImg.format("../upload_file/report/" + obj.report_file_name);
                     }
                     $("#img_list").html("");
                     $("#img_list").html(str);
 
+                    var swiper = new Swiper('.swiper-container',{
+                        effect: "slide",
+                        loop: true,
+                        navigation:{ nextEl: ".slide_next_btn", prevEl: ".slide_prev_btn" },
+                        //autoplay: { delay: 3000 },
+                        on:{ slideChange: function(){
+                            $(".main_visual").removeClass("bounce");
+                            setTimeout(function() {
+                                $(".main_visual").addClass("bounce")
+                            }, 200);
+
+                        }
+                        }
+                    });
+                    swiper.update();
+
                 }catch(e) {
                     console.log(e)
                 }
+
 
             },
             error:function(error) {
@@ -53,7 +53,7 @@
     <div class="img_slider">
         <div class="swiper-container">
             <ul class="swiper-wrapper" id="img_list" >
-                <!-- li class="swiper-slide">
+                <li class="swiper-slide">
                     <img src="../images/img_sample2.jpg" alt="">
                 </li>
                 <li class="swiper-slide">
@@ -67,7 +67,7 @@
                 </li>
                 <li class="swiper-slide">
                     <img src="../images/img_sample4.jpg" alt="">
-                </li -->
+                </li>
             </ul>
         </div>
         <div class="slide_nav_btn">
@@ -76,8 +76,8 @@
         </div>
     </div>
     <div class="btn_box">
-        <button type="button" class="like_btn">26</button>
-        <span class="reply_cnt">15</span>
+        <button type="button" class="like_btn" id="pop_like_cnt">26</button>
+        <span class="reply_cnt" id="pop_comment_cnt">15</span>
     </div>
     <button tpye="button" class="pop_close"></button>
 </div>
--- main1/index.php
+++ main1/index.php
@@ -1,21 +1,8 @@
 <? include $_SERVER['DOCUMENT_ROOT'] . "/include/head.php" ?>
-<?
-/*
-$block = 10;
-$scroll_num = 0;
-$query = "SELECT report.idx AS report_idx, report.wdate, report.content_text, report.report_hashtag, report.likes, (SELECT COUNT(*) AS cnt FROM report_comments WHERE report_idx=report.idx) AS comment_cnt,  member.real_name, member.file_chg  FROM report_list AS report, member_info AS member WHERE report.del_yn='N' AND report.member_idx=member.idx ";
-$query_limit .= $query." LIMIT ".($block*$scroll_num)." , ".$block ;
-$result = mysqli_query($gconnet,$query_limit);
 
-$cnt_result = mysqli_query($gconnet,$query);
-$cnt = mysqli_fetch_all($cnt_result);
-$num = count($cnt);
-*/
-// publ
-?>
 <script>
     var page = 0;
-    var block = 10;
+    var block = 5;
     function getList() {
 
         $.ajax({
@@ -26,8 +13,19 @@ $num = count($cnt);
 
                 try {
                     var res = JSON.parse(response)
+
                 }catch (e) {
                     $("#main_list").append(response);
+
+                    $(".pop_call").on("click",function(){
+                        console.log("pop call")
+                        var name = $(this).attr("data-pop");
+                        $(".popup."+name).fadeIn();
+                        $(".mask").fadeIn();
+                        $("html").addClass("scroll_no");
+                        $(".snb").removeClass("snb_on");
+                    });
+
                     page++;
                 }
 
@@ -38,6 +36,14 @@ $num = count($cnt);
         })
     }
     getList();
+    $(window).on("scroll", function() {
+        var scrollHeight = $(document).height();
+        var scrollPosition = $(window).height() + $(window).scrollTop();
+        if ((scrollHeight - scrollPosition) / scrollHeight === 0) {
+            getList();
+        }
+    });
+
 </script>
 <body>
 <div class="wrapper">
@@ -54,9 +60,6 @@ $num = count($cnt);
 <? include $_SERVER['DOCUMENT_ROOT']."/include/gnb.php" ?>
 <? include $_SERVER['DOCUMENT_ROOT']."/include/etc_popup.php" ?>
 <? include $_SERVER['DOCUMENT_ROOT']."/include/img_popup.php" ?>
-
-
-
 <?include $_SERVER['DOCUMENT_ROOT']."/include/share_pop.php"?>
 </body>
 </html>
