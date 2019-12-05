b760ccdd79a00cad8e3a97c9fe7ce91035ffdd21
munkyuhwan <munkyuhwan@gmail.com>
2019-12-05 09:59:34 +0900

    인트로 수정

--- intro/index.php
+++ intro/index.php
@@ -1,6 +1,6 @@
 <? include $_SERVER['DOCUMENT_ROOT'] . "/include/head.php" ?>
 <?
-
+/*
 $_SESSION['user_access_idx'] = 54;
 
 $query = "SELECT idx, real_name, file_chg FROM member_info WHERE idx=".$_SESSION['user_access_idx'] ;
@@ -11,6 +11,7 @@ $row = mysqli_fetch_assoc($result);
 $_SESSION['user_access_name'] = $row['real_name'];
 $_SESSION['profile_img'] = $row['file_chg'];
 echo "<script>location.replace('../main1');</script>";
+*/
 ?>
 <body>
 <div class="wrapper">
