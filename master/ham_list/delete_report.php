f50177814a9ca1f78fdc675c2db9bf97521aa22a
munkyuhwan <munkyuhwan@gmail.com>
2019-12-05 16:57:40 +0900

    삭제 어떻게 할까 고민중

--- master/ham_list/delete_report.php
+++ master/ham_list/delete_report.php
@@ -4,7 +4,25 @@ $idx = trim(sqlfilter($_REQUEST['idx']));
 
 $query = "UPDATE report_list SET del_yn='Y' WHERE idx=".$idx;
 $result = mysqli_query($gconnet, $query);
-echo $query;
-print_r($result);
+
+/*
+$file_list = "SELECT * FROM report_additional_files WHERE report_idx=".$idx;
+$file_list_result = mysqli_query($gconnet, $file_list);
+
+//게시글의 스크랩 삭제
+$delete_sub_categories = "DELETE FROM scrab_list WHERE report_idx=".$idx;
+$delete_result = mysqli_query($gconnet, $delete_sub_categories);
+
+
+while($row = mysqli_fetch_assoc($file_list_result)) {
+    $delete_query = "DELETE FROM report_additional_files WHERE idx=".$row['idx'];
+    $delete_result = mysqli_query($gconnet, $delete_query);
+
+    if ($delete_result) {
+        unlink("../../upload_file/report/".$row['report_file_name']);
+    }
+
+}
+*/
 
 ?>
