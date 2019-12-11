3158bf90c58eaac8bd5bf52f3de4335b3384d416
munkyuhwan <munkyuhwan@gmail.com>
2019-12-11 14:47:46 +0900

    알림 추가

--- main4/index.php
+++ main4/index.php
@@ -2,7 +2,6 @@
 <?
 $query = "SELECT * FROM alarm_list WHERE member_idx=".$_SESSION['user_access_idx'];
 $result = mysqli_query($gconnet, $query);
-
 ?>
 <body>
 <div class="wrapper">
@@ -12,17 +11,56 @@ $result = mysqli_query($gconnet, $query);
         <div class="alrim_wrap">
             <ul>
                 <?while ( $row = mysqli_fetch_assoc($result) ) {?>
-                    <li class="item">
-                        <div class="alrim_type type1"> <!-- 발행 -->
-                            <img src="../images/img_sample2.jpg" alt="">
-                        </div>
-                        <div class="tlt">내 제보가 [<span>광진구 대신 전해드립니다</span>]에 발행되었습니다.</div>
-                        <div class="desc">얼마전에 구리시로 이사온 24살 남자입니다.
-                            이사온지 얼마 안돼서 친구도 없고 맨날 심심해서 혼자 피시방
-                            만 다니는데 같이 다닐 친구필요해요 ㅠ  남자여자 상관없고
-                            좋아요 누르면 달려갑니다!  </div>
-                        <div class="date">8월 20일 오후 6:18</div>
-                    </li>
+                    <?
+                    if ($row['alarm_type'] == "PUBL") {
+                        $detail_query = "SELECT report.content_text, categories.category_name, categories.profile_img, report.wdate FROM report_list AS report, report_categories AS categories ";
+                        $detail_query .= "  WHERE report.category=categories.idx ";
+                        $detail_query .= " AND report.idx=" . $row['report_idx'];
+
+                        $detail_result = mysqli_query($gconnet, $detail_query);
+                        $detail_row = mysqli_fetch_assoc($detail_result);
+
+                        $alarm_title = $detail_row['category_name'];
+                        $alarm_content = $detail_row['content_text'];
+                        ?>
+                        <li class="item">
+                            <div class="alrim_type type1"> <!-- 발행 -->
+                                <img src="../upload_file/category_profile/<?=$detail_row['profile_img']?>" alt="">
+                            </div>
+                            <div class="tlt">내 제보가 [<span><?=$alarm_title?></span>]에 발행되었습니다.</div>
+                            <div class="desc"><?=$alarm_content?> </div>
+                            <div class="date"><?=date("m월 d일 H:i", strtotime($detail_row['wdate']))?></div>
+                        </li>
+
+                    <?} else if ($row['alarm_type'] == "LIKE") {?>
+                        <?
+                        $detail_query = "SELECT content_text, likes, wdate FROM report_list WHERE idx=".$row['report_idx'];
+                        $detail_result = mysqli_query($gconnet, $detail_query);
+                        $detail_row = mysqli_fetch_assoc($detail_result);
+                        ?>
+                        <li class="item">
+                            <div class="alrim_type type2"> <!-- 좋아요 -->
+                                <img src="../images/icon_heart.png" alt="" width="32">
+                            </div>
+                            <div class="tlt">내 제보를 <span><?=$detail_row['likes']?>명</span>이 마음에 들어합니다.</div>
+                            <div class="desc"><?=$detail_row['content_text']?></div>
+                            <div class="date"><?=date("m월 d일 H:i", strtotime($detail_row['wdate']))?></div>
+                        </li>
+                    <?}else if ($row['alarm_type'] == "TOP") {?>
+                        <?
+                        $detail_query = "SELECT content_text, wdate FROM report_list WHERE idx=".$row['report_idx'];
+                        $detail_result = mysqli_query($gconnet, $detail_query);
+                        $detail_row = mysqli_fetch_assoc($detail_result);
+                        ?>
+                        <li class="item">
+                            <div class="alrim_type type3"> <!-- 인기등록 -->
+                                <img src="../images/icon_popular.png" alt="" width="20">
+                            </div>
+                            <div class="tlt">내 제보가 인기 게시물에 등록되었습니다!</div>
+                            <div class="desc"><?=$detail_row['content_text']?></div>
+                            <div class="date"><?=date("m월 d일 H:i", strtotime($detail_row['wdate']))?></div>
+                        </li>
+                    <?}?>
                 <?}?>
             </ul>
         </div>
@@ -36,7 +74,3 @@ $result = mysqli_query($gconnet, $query);
 
 </body>
 </html>
-
-<!--
-
--->
