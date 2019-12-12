f641641f1bc8bb3d426a07f0ec96b86bd411ea0a
munkyuhwan <munkyuhwan@gmail.com>
2019-12-12 17:11:47 +0900

    인기 피드 올라갈때 알림

--- include/extra_functions.php
+++ include/extra_functions.php
@@ -25,6 +25,32 @@ function addToAlarm($alarmType, $reportIdx, $receiver, $alarmMsg, $gconnet) {
 
 }
 
+function checkPopular($reportIdx, $gconnet) {
+
+    $cnt_query = "SELECT report.member_idx, report.likes, report.view_cnt, (SELECT COUNT(*)  FROM report_comments WHERE report_idx=report.idx ) AS comment_cnt FROM report_list AS report WHERE report.idx=".$reportIdx;
+    $cnt_result = mysqli_query($gconnet, $cnt_query);
+    $cnt_data = mysqli_fetch_assoc($cnt_result);
+
+    $report_likes = $cnt_data['likes'];
+    $report_view_cnt = $cnt_data['view_cnt'];
+    $report_comment_cnt = $cnt_data['comment_cnt'];
+    $member_idx = $cnt_data['member_idx'];
+
+    $check_popularity = "SELECT COUNT(*) AS cnt FROM popular_feeds WHERE category_idx=".$reportIdx;
+    $check_popularity .= " AND view_cnt > ".$report_view_cnt.', ';
+    $check_popularity .= " AND comment_cnt > ".$report_comment_cnt.", ";
+    $check_popularity .= " AND like_cnt > ".$report_likes." ";
+
+    $popularity_result = mysqli_query($gconnet, $check_popularity);
+    $popularity_data = mysqli_fetch_assoc($popularity_result);
+
+    if ( intval($popularity_data['cnt']) > 0 ) {
+        addToAlarm("TOP", $reportIdx, $member_idx, "", $gconnet);
+    }
+
+
+}
+
 
 
 ?>
