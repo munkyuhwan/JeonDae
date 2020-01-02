3adb3f60e28f876cc7e05f00bd442ccb305c9edc
munkyuhwan <munkyuhwan@gmail.com>
2020-01-02 12:13:45 +0900

    로고 클릭 수정

--- include/header.php
+++ include/header.php
@@ -1,6 +1,6 @@
 <header>
     <div class="header grd_bg">
-        <h1><a href="<?=$_SERVER['DOCUMENT_ROOT']?>/main1"><img src="../images/logo_txt.png" alt="전대전 - 전국 대신 전해드립니다"></a></h1>
+        <h1><a href="../main1"><img src="../images/logo_txt.png" alt="전대전 - 전국 대신 전해드립니다"></a></h1>
         <button type="button" class="snb_btn"></button>
     </div>
 </header>
